39ce9897d580d6af914c4f33e16d353c9f48902f
Dayvid Victor <victor.dvro@gmail.com>
2011-05-17 18:43:21 -0300

    distance.py - commented

--- learning_machine/distance.py
+++ learning_machine/distance.py
@@ -5,27 +5,28 @@ def is_unknown(att):
 	return att == '?'
 
 
-'''
-function that takes the euclidian distance of two vectors of numerical data.
-data_range can be defined using the function get_data_range.
-'''
 def euclidian_distance(a, b, data_range = []):
+	'''
+	Returns the euclidian distance of two vectors of numerical data.
+	data_range can be defined using the function get_data_range.
+	'''
 	if data_range == []:
 		data_range = [1 for i in range(len(a))]
 	
 	return sum([euclidian_distance_i(ai, bi, ri)**2 for ai, bi, ri in zip(a,b, data_range)])
 
-'''
-function that takes de euclidian distance of an atribute i.
-'''
+
 def euclidian_distance_i(num_a, num_b, num_range = 1):
+	'''
+	Returns de euclidian distance of an atribute i.
+	'''
 	return abs((float(num_a) - float(num_b))/num_range)
 
 
-'''
-function that takes the hamming distance of two vectors with categorical data.
-'''
 def hamming_distance(a, b):
+	'''
+	Returns the hamming distance of two vectors with categorical data.
+	'''
 	return sum(hamming_distance_i(ai, bi) for ai,bi in zip(a,b))
 
 def hamming_distace_i(ai,bi):
@@ -33,7 +34,7 @@ def hamming_distace_i(ai,bi):
 
 def vdm(a, b, training):
 	'''
-	Function that takes the vdm distance of two vectors with categorical data.
+	Returns the vdm distance of two vectors with categorical data.
 	The VDM (Value Difference Metric) finds out when two values have the same
 	distribution over some class.
 
@@ -43,11 +44,14 @@ def vdm(a, b, training):
 	'''
 	return math.sqrt(sum([vdm_i(i, ai, bi) for i, ai, bi in zip(range(len(a)),a,b)]))
 
-'''
-NOT TESTED
 
-'''
+
 def vdm_i(i, ai, bi, training, q = 2):
+	'''
+	Returns the vdm distance of a single atribute.
+	
+	NOT TESTED
+	'''
 	classes = set([e[-1] for e in training])
 	
 	ai_count_class = [len(filter(lambda e: e[i] == ai and e[-1] == c, training)) for c in classes]
@@ -70,22 +74,22 @@ def vdm_discretized(a, b, training):
 def vdm_interpolad(a, b, training):
 	pass #TODO
 
-'''
-Heterogeneous Euclidian-Overlap Metric
-
-Vector distance for heterogeneous data.
-HEOM uses euclidian distance for numerical data
-and hamming distance for categorical data.
-
-function parameters:
-	a - vector to compare
-	b - vector to compare
-	data_nature - the data nature of the attributes of de vectors to be compared
-		      it is 'n' for numerical and 'c' for categorical.
-		      if not specified it will assume that the numerical is the official measure
-
-'''
 def heom(a, b, data_range = [], data_nature = []):
+	'''
+	Heterogeneous Euclidian-Overlap Metric
+
+	Vector distance for heterogeneous data.
+	HEOM uses euclidian distance for numerical data
+	and hamming distance for categorical data.
+
+	function parameters:
+		a - vector to compare
+		b - vector to compare
+		data_nature - the data nature of the attributes of de vectors to be compared
+			      it is 'n' for numerical and 'c' for categorical.
+			      if not specified it will assume that the numerical is the official measure
+
+	'''
 
 	if len(data_range) == 0:
 		data_range = [1 for i in range(len(a))]
@@ -97,7 +101,9 @@ def heom(a, b, data_range = [], data_nature = []):
 	
 	
 def heom_i(ai, bi, att_range, nature):
-	
+	'''
+	Returns the HEOM of one single atribute.
+	'''
 	if is_unknown(ai) and is_unknown(bi):
 		return 0
 	elif is_unknown(ai) or is_unknown(bi):
@@ -110,4 +116,15 @@ def heom_i(ai, bi, att_range, nature):
 
 	return 0
 
+
+def hvdm(a, b, data_range = [], data_nature = []):
+	pass #TODO
+
+
+def dvdm(a, b, data_range = [], data_nature = []):
+	pass #TODO
+
+
+def ivdm(a, b, data_range = [], data_nature = []):
+	pass #TODO
 
