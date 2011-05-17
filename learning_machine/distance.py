fa8139985b743ce734358ee2de8a5ff5d432d048
Dayvid Victor <victor.dvro@gmail.com>
2011-05-17 18:02:55 -0300

    distance.py - vdm.py added

--- learning_machine/distance.py
+++ learning_machine/distance.py
@@ -30,12 +30,31 @@ The VDM (Value Difference Metric) finds out when two values have the same
 distribution over some class.
 
 Is usually better than Hamming Distance.
+
+NOT TESTED
 '''
-def vdm(a, b):
-	pass
+def vdm(a, b, training):
+	return math.sqrt(sum([vdm_i(i, ai, bi) for i, ai, bi in zip(range(len(a)),a,b)]))
 
+'''
+NOT TESTED
 
+'''
+def vdm_i(i, ai, bi, training, q = 2):
+	classes = set([e[-1] for e in training])
+	
+	ai_count_class = [len(filter(lambda e: e[i] == ai and e[-1] == c, training)) for c in classes]
+	bi_count_class = [len(filter(lambda e: e[i] == bi and e[-1] == c, training)) for c in classes]
 
+	n_ac = sum(ai_count_class)
+	n_bc = sum(bi_count_class)
+		
+	vdmi = 0
+	for za, zb in zip(ai_count_class, bi_count_class):
+		diff = float(za)/n_ac - float(zb)/n_bc
+		vdmi = vdmi + (abs(diff)**q)
+
+	return vdmi
 
 
 
