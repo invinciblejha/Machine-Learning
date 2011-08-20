dec30674591098df21afdfa4f0e5bc3a2cbc86a3
Dayvid Victor <dayvid@dayvid.itautec.cin.ufpe.br>
2011-08-20 16:30:43 -0300

    numerical.py with euclidian_distance tested

---
+++ machine_learning/distance/numerical.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+import numpy as np
+
+def euclidian_distance(a, b, range_vector = None):
+    """
+
+    """
+    if range_vector == None:
+        range_vector = np.tile([1], len(a))
+
+    return map(lambda e: e ** 2, a - b) / range_vector
+
